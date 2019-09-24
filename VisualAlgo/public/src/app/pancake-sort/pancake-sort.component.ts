a7db80b9db823212375f62c7c544aa73509addd2
Ryan Baermann <rcbaermann@gmail.com>
2019-09-24 11:48:45 -0700

    got all the basic algos into angular

--- VisualAlgo/public/src/app/pancake-sort/pancake-sort.component.ts
+++ VisualAlgo/public/src/app/pancake-sort/pancake-sort.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as p5 from 'p5';
 
 @Component({
   selector: 'app-pancake-sort',
@@ -7,12 +8,93 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PancakeSortComponent implements OnInit {
 
+  values = [];
+
+  w = 100;
+  sorted = false;
+  arr = [0, 0, 0, 0];
+
   constructor() { }
 
   ngOnInit() {
+    const sketch = (s) => {
+      s.preload = () => {
+
+      }
+
+      s.setup = () => {
+        s.createCanvas(window.innerWidth - 20, window.innerHeight - 120);
+        this.values = new Array(s.floor(s.width / this.w));
+        for(let i = 0; i < this.values.length; i++) {
+          this.values[i] = s.random(s.height - 50);
+        }
+      }
+
+      s.draw = () => {
+        s.background(255);
+        if(this.sorted == false) {
+          this.arr = this.pancakeSort(this.arr);
+          if(this.arr[0] > this.values.length - 1) {
+            this.sorted = true;
+          }
+        }
+
+        for(let i = 0; i < this.values.length; i++) {
+          s.stroke(200);
+          if(this.arr[0] < this.values.length - i) {
+            s.fill(51);
+          }
+          else {
+            s.fill(0, 128, 0);
+          }
+          if(this.arr[2] == i && this.arr[0] < this.values.length - i) {
+            s.fill(128, 0, 0);
+          }
+          s.rect(i * this.w, s.height - this.values[i], this.w, this.values[i]);
+        }
+        
+      }
+    }
+
+    let canvas = new p5(sketch);
   }
 
-  Go() {
-    
+  pancakeSort(arr) {
+    let i = arr[0];
+    let j = arr[1];
+    let max = arr[2];
+    let flipped = arr[3];
+    if(flipped == 0) {
+      if(this.values[max] < this.values[j]) {
+        max = j;
+      }
+      if(i < this.values.length) {
+        j += 1;
+        if(j >= this.values.length - i) {
+          j = 0;
+          this.flipSection(this.values, 0, max);
+          flipped = 1;
+          max = 0;
+        }
+      }
+    }
+    else {
+      this.flipSection(this.values, 0, this.values.length - 1 - i);
+      i += 1;
+      flipped = 0;
+    }
+    arr[0] = i;
+    arr[1] = j;
+    arr[2] = max;
+    arr[3] = flipped;
+    return arr;
+  };
+
+  flipSection(arr, a, b) {
+    for(let i = a; i < b / 2; i++) {
+        let temp = arr[i];
+        arr[i] = arr[b - i];
+        arr[b - i] = temp;
+    }
   }
 }
--- VisualAlgo/public/src/app/selection/selection.component.ts
+++ VisualAlgo/public/src/app/selection/selection.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as p5 from 'p5';
 
 @Component({
   selector: 'app-selection',
@@ -7,9 +8,81 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectionComponent implements OnInit {
 
+  values = [];
+
+  w = 100;
+  sorted = false;
+  arr = [0, 0, 0, 0];
+
   constructor() { }
 
   ngOnInit() {
+    const sketch = (s) => {
+      s.preload = () => {
+
+      }
+
+      s.setup = () => {
+        s.createCanvas(window.innerWidth - 20, window.innerHeight - 120);
+        this.values = new Array(s.floor(s.width / this.w));
+        for(let i = 0; i < this.values.length; i++) {
+          this.values[i] = s.random(s.height - 50);
+        }
+      }
+
+      s.draw = () => {
+        s.background(255);
+        if(this.sorted == false) {
+          this.arr = this.selectionSort(this.arr);
+          if(this.arr[0] > this.values.length - 1) {
+            this.sorted = true;
+          }
+        }
+
+        for(let i = 0; i < this.values.length; i++) {
+          s.stroke(200);
+          if(this.arr[0] <= 0 + i) {
+            s.fill(51);
+          }
+          else {
+            s.fill(0, 128, 0);
+          }
+          if(this.arr[2] == i) {
+            s.fill(128, 0, 0);
+          }
+          s.rect(i * this.w, s.height - this.values[i], this.w, this.values[i]);
+        }
+      }
+    }
+
+    let canvas = new p5(sketch);
   }
 
+  swap(arr, a, b) {
+    let temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+  }
+
+  selectionSort(arr) {
+    let i = arr[0];
+    let j = arr[1];
+    let temp = arr[2];
+    if(this.values[temp] > this.values[j]) {
+      temp = j;
+    }
+    if(i < this.values.length) {
+      j += 1;
+      if(j > this.values.length - 1) {
+        this.swap(this.values, i, temp);
+        i += 1;
+        j = i;
+        temp = i;
+      }
+    }
+    arr[0] = i;
+    arr[1] = j;
+    arr[2] = temp;
+    return arr;
+  };
 }
